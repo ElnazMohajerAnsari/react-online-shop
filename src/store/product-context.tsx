96be9b29b56ad51d498d216359a44fe9c2d83a28
ElnazMohajerAnsari <103626555+ElnazMohajerAnsari@users.noreply.github.com>
2023-06-13 21:50:27 +0330

    Edit product-context.tsx

--- src/store/product-context.tsx
+++ src/store/product-context.tsx
@@ -11,18 +11,12 @@ interface ProductItemType {
 
 interface ProductContextType {
   items: ProductItemType[];
-  //   totalAmount: number;
   addItem: (item: ProductItemType) => void;
-  //   decreaseItem: (id: string) => void;
-  //   deleteItem: (id: string) => void;
 }
 
 const ProductContext = createContext<ProductContextType>({
   items: [],
-  //   totalAmount: 0,
   addItem: (item: ProductItemType) => {},
-  //   decreaseItem: (id: string) => {},
-  //   deleteItem: (id: string) => {},
 });
 
 export default ProductContext;
